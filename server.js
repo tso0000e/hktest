b086f72847e05f40a46697649ea46289c9ecad83
tso0000e <tso0000e@gmail.com>
2017-03-31 19:23:02 +0800

    serverjsfix

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 // app.js
 var express = require('express');  
 var app = express();  
-var server = require('http').createServer(app);  
+//var server = require('http').createServer(app);  
 var io = require('socket.io')(server);
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static(__dirname + '/node_modules'));  
-app.get('/', function(req, res,next) {  
+
+app.get('/', function(req, res) {  
     res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(8080);  
+//server.listen(8080);  
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
