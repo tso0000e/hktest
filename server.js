b6c0159e722384f78f1eba64ad24af535510720f
tso0000e <tso0000e@gmail.com>
2017-03-31 19:27:38 +0800

    serverfix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');  
 var app = express();  
 //var server = require('http').createServer(app);  
-var io = require('socket.io')(server);
+//var io = require('socket.io')(server);
 
 app.set('port', (process.env.PORT || 5000));
 
